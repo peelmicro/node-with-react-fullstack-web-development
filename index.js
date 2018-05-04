88a780d11807c920a7ec5ee84a73f5de49821be0
RIM\Juan.Pablo.Perez <juan.pablo@retailinmotion.com>
2018-05-04 15:39:13 +0100

    added billing and client side app!

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'))
+  // Express will serve up index.html file 
+  // If it doesn't recognize the rouer
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
 
